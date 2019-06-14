2bb2679b5aa294e55b9adaa318d9e8d490d0d9ba
Maria Yalpani <maria@yalpani.de>
2019-06-14 10:03:11 +0200

    changed database string

--- models.py
+++ models.py
@@ -62,8 +62,6 @@ class Book(Base):
         }
 
 
-engine = create_engine('postgresql://catalog:'
-                       + os.environ.get('PGPASS')
-                       + '@localhost:5432/catalog')
+engine = create_engine('postgresql://catalog:library2019@localhost:5432/catalog')
 
 Base.metadata.create_all(engine)
